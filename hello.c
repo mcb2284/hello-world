b3fa0fbd8e4ebc7073e0f1362f98aa39ec87bc3f
Michael <61466734+mcb2284@users.noreply.github.com>
2020-10-19 17:20:48 -0400

    Created hello world file

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    //Print Hello World
+    printf("Hello World!\n");
+    
+
+    //Print the numbers 0 - 15
+    for(int x = 0; x <= 15; x++){
+        printf("%d\n", x);
+    }
+}
